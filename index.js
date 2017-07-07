67af8865449f82fbf1b4b390509ab9a1b5b6016b
Mirko May <mail@mirkomay.com>
2017-07-08 03:21:34 +1200

    Fixed css

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ module.exports = {
   name: 'ember-image-scrubber',
 
   included: function(app) {
-    this._super.included(app);
+    this._super.included.apply(this, arguments);
+    // app.import(app.bowerDirectory + '/x-button/vendor/css/x-button.css');
     app.import('vendor/ember-image-scrubber.css');
   }
 
