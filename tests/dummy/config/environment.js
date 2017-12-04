aad48ebd9200589b78702ae8f2308edd8fce023c
Mirko May <mail@mirkomay.com>
2017-12-04 13:20:24 +1300

    Dummy app

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    rootURL: '/',
+    rootURL: '/ember-more-menu',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
